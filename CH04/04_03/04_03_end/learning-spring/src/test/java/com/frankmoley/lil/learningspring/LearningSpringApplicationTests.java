ecf54394062ec0b9d1017afc8f9df378c6a9c919
Chanuka Dinuwan <chanukadinuwan35@gmail.com>
2022-07-17 23:19:29 +0530

    Initial commit

---
+++ CH04/04_03/04_03_end/learning-spring/src/test/java/com/frankmoley/lil/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankmoley.lil.learningspring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
